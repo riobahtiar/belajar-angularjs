e6d46984b61ec5f6cb576d90ea4f2c5f600131ab
riobahtiar <akhibahtiar@gmail.com>
2015-08-27 12:22:59 +0700

    add assets

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	watch = require('gulp-watch'),
 	sourcemaps = require('gulp-sourcemaps'),
-	rename = require("gulp-rename");
+	rename = require("gulp-rename"),
+	jsmin = require('gulp-jsmin'),
+	imagemin = require('gulp-imagemin'),
+	pngquant = require('imagemin-pngquant');
 
 	// SASS
 gulp.task('sass', function () {
@@ -11,12 +14,23 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('assets/css-min'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('.assets/scss/**/*.scss', ['sass']);
+	// Gulp JSmin
+gulp.task('jsmin', function () {
+    gulp.src('assets/js/**/*.js')
+        .pipe(jsmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('assets/js-min'));
 });
-	// Gulp WATCH
-
+	// Gulp Img Min
+gulp.task('imagemin', function () {
+    return gulp.src('assets/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('assets/img-min'));
+});
 	// Default Action
-gulp.task('default',['sass'], function() {
-  // place code for your default task here
+gulp.task('default',['sass','jsmin','imagemin'], function() {
 });
